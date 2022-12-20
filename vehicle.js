5acc3e69c4492b901cbadbec538571551321364a
Laura Brea <laurabrea17@gmail.com>
2022-12-20 16:44:34 -0500

    added class vehicle and extended it to class car

--- vehicle.js
+++ vehicle.js
@@ -1,88 +1,34 @@
 class Vehicle {
-    constructor(make, model, year, color, mileage) {
-        this.make = make;
-        this.model = model;
-        this.year = year;
-        this.color = color;
+    constructor(maximumPassengers,passenger,numberOfWheels,maximumSpeed,fuel,scheduleService){
+        this.maximumPassengers = 5;
         this.passenger = 0;
-        this.speed = 0;
-        this.mileage = mileage;
-        this.started = false;
-        this.numberOfWheels = 0;
+        this.numberOfWheels= 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+loadPassenger(){
+    if(this.passenger < this.maximumPassengers){
+        console.log('There is room available')
+    }else{
+        console.log('This car is full')
+    }
+    
+}
+
+start(){
+    if(this.fuel > 0){
+        console.log("Start the car")
+    }else {
+        console.log("You need fuel to start")
     }
 
-    start() {
-        if (this.fuel > 0) {
-            return this.started = true;
-            console.log("engine started...!!!");
-        } else {
-            return this.started = false;
-            console.log("engine cannot start...");
-        }
-    }
-    accelerate() {
-        if (this.started) {
-            if (this.fuel > 0) {
-                console.log(this.speed += 1);
-                this.fuel = this.fuel - 1;
-            } else {
-                console.log("out of fuel.");
-                this.stop();
-            }
-        } else {
-            alert("You need to start the engine first.");
-        }
-    }
-    decelerate() {
-        if (this.started) {
-            if (this.fuel > 0) {
-                if (this.speed > 0) {
-                    console.log(this.speed -= 1);
-                    this.fuel = this.fuel - 1;
-                } else {
-                    console.log(this + " has stopped moving");
-                    this.fuel = this.fuel - 1;
-                }
-            } else {
-                console.log("out of fuel.");
-                this.stop();
-            }
-        } else {
-            alert("You need to start the engine first.");
-        }
-    }
-    stop() {
-        this.started = false;
-    }
+}
 
-    drive() {
-        accelerate();
-    }
-    brake() {
-        decelerate();
-    }
+scheduleService(){
+    
+}
 
-    autoPark()
-    {
-
-    }
-
-    autoDrive()
-    {
-      
-    }
-
-    typeOfVehicle(wheels) {
-        if (this.numberOfWheels == 8 && 8 == wheels) {
-            console.log(this.model + " " + this.make + " is a Truck");
-        } else if (this.numberOfWheels == 4 && 4 == wheels) {
-            console.log(this.model + " " + this.make + " is a CAr");
-        } else if (this.numberOfWheels == 2 && 2 == wheels) {
-            console.log(this.model + " " + this.make + " is a Bike");
-        } else {
-            console.log("Unknown type of vehicle");
-        }
-    }
 }
 
 //This exports things you want to use from this "module", more info in readme
